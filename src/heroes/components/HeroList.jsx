81cc3ab1a72c859535348657f6a992b9d090f6b4
Juan <gurquiza8@gmail.com>
2023-05-20 22:46:57 -0500

    Primer commit

---
+++ src/heroes/components/HeroList.jsx
@@ -0,0 +1,18 @@
+import { HeroCard } from './';
+import { getHeroesByPublisher } from '../helpers';
+
+export const HeroList = ({ publisher }) => {
+
+    const heroes = getHeroesByPublisher(publisher);
+    console.log(heroes);
+
+    return (
+        <div className='row rows-cols-1 row-cols-md-3 g-3'>
+            {
+                heroes.map( hero => (
+                    <HeroCard key={hero.id} {...hero} />
+                ))
+            }
+        </div>
+    )
+}
---
+++ src/heroes/pages/index.js
@@ -0,0 +1,9 @@
+
+
+export * from './DcPage';
+export * from './MarvelPage';
+export * from './SearchPage';
+export * from './HeroPage';
+
+
+

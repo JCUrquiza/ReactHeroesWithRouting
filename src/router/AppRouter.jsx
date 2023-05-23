96f9ae0bce176e19ead82b5d3dd1a93e06e464f6
Juan <gurquiza8@gmail.com>
2023-05-22 23:44:12 -0500

    Agregando ruta publica

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,14 +2,22 @@ import { Route, Routes } from 'react-router-dom';
 import { HeroesRoutes } from '../heroes';
 import { LoginPage } from '../auth';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
 
     return (
         <>
             <Routes>
-                <Route path="/login" element={ <LoginPage /> } />
 
+                {/* Rutas publicas */}
+                <Route path="/login" element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>
+                } />
+                {/* <Route path="/login" element={ <LoginPage /> } /> */}
+                
                 {/* Rutas privadas */}
                 <Route path="/*" element={ 
                     <PrivateRoute>
@@ -17,7 +25,7 @@ export const AppRouter = () => {
                     </PrivateRoute>
                 } />
                 {/* <Route path="/*" element={ <HeroesRoutes /> } /> */}
-                
+
             </Routes>
         </>
     )
